cc5857969afcb2863d825f588a7cc98dc7d3c89b
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-04 13:36:33 -0400

    added solutions for java basics examples

--- u2-java-content/src/main/java/java_basics/HashSetExamples.java
+++ u2-java-content/src/main/java/java_basics/HashSetExamples.java
@@ -5,52 +5,68 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 
-public class HashSets {
+public class HashSetExamples {
 
     public static void main(String[] args) {
 
         /*
             A HashSet doesn't have key/value pairs like a HashMap.
             It is more like an ArrayList, but it does not allow duplicate elements.
+            It also does not store its elements in any particular order (use a LinkedHashSet instead)
          */
 
         /* CREATING A HASHSET AND ADDING KEY/VALUE PAIRS */
 
         // TODO: Create an empty HashSet called numSet
-
+        HashSet<Integer> numSet = new HashSet<>();
 
         // TODO: Add several numbers to the HashSet, including some duplicates
-
+        numSet.add(3);
+        numSet.add(24);
+        numSet.add(8);
+        numSet.add(12);
+        numSet.add(3);
+        numSet.add(65);
+        numSet.add(1);
+        numSet.add(8);
 
         // TODO: Print the HashSet to the console. What do you see?
-
+        System.out.println(numSet); // no duplicate elements!
 
 
         /* REMOVING ELEMENTS FROM A HASHSET */
 
         // TODO: Remove one of the numbers and print it again
-
+        numSet.remove(24);
+        System.out.println(numSet);
 
 
         /* CHECKING FOR EXISTING ELEMENT */
 
         // TODO: Check to see if the HashSet contains certain numbers and print the result
         // Check both one you know is there and one you know is NOT
-
+        System.out.println(numSet.contains(3)); // true
+        System.out.println(numSet.contains(24)); // false
 
 
         /* LOOPING OVER ELEMENTS IN A HASHSET */
 
         // TODO: Iterate over the elements and print whether they're even or not
         //  (e.g. "The number 6 is even.")
-
+        for (int num : numSet) {
+            String evenText = num % 2 == 0 ? "even" : "not even";
+            System.out.println("The number " + num + " is " + evenText + ".");
+        }
 
         /* BONUS KNOWLEDGE: SORTING HASHSETS */
 
         // TODO: Convert the HashSet to an ArrayList
         //  then sort it with Collections.sort()
         //  then convert it back to a LinkedHashSet and print it
-
+        ArrayList<Integer> numList = new ArrayList<>(numSet);
+        Collections.sort(numList);
+        LinkedHashSet<Integer> sortedNumSet = new LinkedHashSet<>(numList);
+        System.out.println(sortedNumSet);
     }
 
 }
--- u2-java-content/src/main/java/java_basics/Main.java
+++ u2-java-content/src/main/java/java_basics/Main.java
@@ -1,6 +1,6 @@
 package java_basics;
 
-// TODO: import Scanner from java.util
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,41 +8,72 @@ public class Main {
 
         /* PRINTING TO THE CONSOLE */
 
-        // TODO: Use System.out to print to the console
+        System.out.println("Welcome to Unit 2!");
 
 
         /* DECLARING AND INITIALIZING VARIABLES */
 
-        // TODO: Declare and initialize several variables and print them to the console
+        String newFaveShow = "Critical Role";
+        String faveActor = "Laura Bailey";
+        String favePC = "Jester";
+        String faveNPC = "Pumat Sol";
+
+        System.out.println("My new favorite show is " + newFaveShow + ", starring " + faveActor + " as " + favePC + ". My favorite NPC is " + faveNPC);
+
+        char letter = 'a';
+        char digit = '9'; // NEIN! (A Critical Role joke)
+        char punctuation = '!';
+        char newline = '\n';
+
+        System.out.println(letter + digit + punctuation + newline);
+
+        int niceRoundNumber = 7;
+        // float aDecNumber = 1.2345;
+        float smallDecNumber = 28.1f; // adding f casts it to float
+        float anotherSmallDecNumber = (float) -1.2394; // this also casts it to float
+        double largeDecNumber = 13.4930295423951;
+
+        System.out.println(niceRoundNumber);
+        System.out.println(smallDecNumber);
+        System.out.println(anotherSmallDecNumber);
+        System.out.println(largeDecNumber);
+
+        boolean isEven = false;
+
+        System.out.println(isEven);
 
 
         /* WRITING METHODS */
 
-        // TODO: After coding your method below main(), call it here and print the result
+        System.out.println(getFullName("Matt", "Mercer"));
 
 
         /* USING METHODS FROM OTHER CLASSES */
 
-        // TODO: Call each of the static methods from the Examples class
+        Methods.sayHello("Bob");
+
+        System.out.println(Methods.addThreeNums(2, 5, 9));
 
 
         /* USING SCANNER TO GET USER INPUT */
 
-        // TODO: Declare and initialize a Scanner object using System.in
+        Scanner input = new Scanner(System.in);
 
-        // TODO: Get user input
         // Precede your input with output to the user so they know what you need
-
+        System.out.println("Enter a whole number: ");
         // Receive the input and store it in a variable
-
+        int userNumber = input.nextInt();
         // Do something with the stored user input
+        System.out.println("Your number is: " + userNumber);
 
         // Always close the Scanner object at the end of the main method
         // for efficiency and security
-        // TODO: Close Scanner
-
+        input.close();
     }
 
-    // TODO: Code a method to return a full name as a String using firstName and lastName
+    // Make sure this is coded outside the main method (convention is to put it below main())
+    public static String getFullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
 
 }
