682c2db5af491b93c30071054e05abd2834c3c46
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-04 13:19:59 -0400

    added HashSet examples

---
+++ u2-java-content/src/main/java/java_basics/HashSets.java
@@ -0,0 +1,56 @@
+package java_basics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class HashSets {
+
+    public static void main(String[] args) {
+
+        /*
+            A HashSet doesn't have key/value pairs like a HashMap.
+            It is more like an ArrayList, but it does not allow duplicate elements.
+         */
+
+        /* CREATING A HASHSET AND ADDING KEY/VALUE PAIRS */
+
+        // TODO: Create an empty HashSet called numSet
+
+
+        // TODO: Add several numbers to the HashSet, including some duplicates
+
+
+        // TODO: Print the HashSet to the console. What do you see?
+
+
+
+        /* REMOVING ELEMENTS FROM A HASHSET */
+
+        // TODO: Remove one of the numbers and print it again
+
+
+
+        /* CHECKING FOR EXISTING ELEMENT */
+
+        // TODO: Check to see if the HashSet contains certain numbers and print the result
+        // Check both one you know is there and one you know is NOT
+
+
+
+        /* LOOPING OVER ELEMENTS IN A HASHSET */
+
+        // TODO: Iterate over the elements and print whether they're even or not
+        //  (e.g. "The number 6 is even.")
+
+
+        /* BONUS KNOWLEDGE: SORTING HASHSETS */
+
+        // TODO: Convert the HashSet to an ArrayList
+        //  then sort it with Collections.sort()
+        //  then convert it back to a HashSet and print it
+
+    }
+
+}
