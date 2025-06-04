37a9dd7b8959991f9c2bbfcaaacc78b1d2ef43a8
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-04 10:24:53 -0400

    added TODOs for first demo in Java basics

---
+++ u2-java-content/src/main/java/java_basics/Methods.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class Methods {
+
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static int addThreeNums(int a, int b, int c) {
+        return a + b + c;
+    }
+}
