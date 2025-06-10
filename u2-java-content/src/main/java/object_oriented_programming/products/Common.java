fdd25ab9ecefe97f3c66f14f461ab1e0254cefc0
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-09 21:53:01 -0400

    added OOP products examples

---
+++ u2-java-content/src/main/java/object_oriented_programming/products/Common.java
@@ -0,0 +1,9 @@
+package object_oriented_programming.products;
+
+public class Common {
+
+    public static final String newline = System.lineSeparator();
+
+    public static final String pipe = " | ";
+
+}
