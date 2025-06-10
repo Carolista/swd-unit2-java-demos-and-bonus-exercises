6b59d41ff39950128ff425c063b89e8ce1b15b23
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-09 21:43:44 -0400

    added oop products example

---
+++ u2-java-content/src/main/java/object_oriented_programming/products/books/UsedBook.java
@@ -0,0 +1,30 @@
+package object_oriented_programming.products.books;
+
+import object_oriented_programming.products.Common;
+
+public class UsedBook extends Book {
+
+    private final String condition;
+
+    public UsedBook(String name, String desc, double price, int quantity, String author, String genre, String format, String condition) {
+        super(name, desc, price, quantity, author, genre, format);
+        this.condition = condition;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Condition: " + condition + Common.newline;
+    }
+
+    @Override
+    public String describe() {
+        return getName() + " by " + getAuthor() + Common.newline +
+                getDesc() + Common.newline +
+                getGenre() + Common.pipe + getFormat() + Common.pipe + condition + Common.pipe + getPrice() + Common.newline;
+    }
+}
---
+++ u2-java-content/src/main/java/object_oriented_programming/products/merchandise/Merchandise.java
@@ -0,0 +1,34 @@
+package object_oriented_programming.products.merchandise;
+
+import object_oriented_programming.products.*;
+
+// A subclass can also be abstract!
+
+public abstract class Merchandise extends Product {
+
+    private final String category;
+    private final String theme;
+
+    public Merchandise(String name, String desc, double price, int quantity, String category, String theme) {
+        super(name, desc, price, quantity);
+        this.category = category;
+        this.theme = theme;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Category: " + category + Common.newline +
+                "Theme: " + (theme.isBlank() ? "(none)" : theme) + Common.newline;
+    }
+
+    public abstract String describe();
+}
