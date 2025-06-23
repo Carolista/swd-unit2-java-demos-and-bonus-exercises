4ac53b1f287ffb1afe9885eceb4720dbdd7299fc
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-23 15:15:11 -0400

    added lesson 2 examples

---
+++ sql-queries/lesson-2-examples.sql
@@ -0,0 +1,24 @@
+-- Create a new table, artist, with an id, first_name, last_name, full_name, and nationality
+-- Add UQ and AI properties for id column
+-- Use G property for full_name and use an expression to auto-generate it from first_name and last_name
+
+-- Add records for van Gogh, da Vinci, Vermeer, Dalí and Monet (ids will auto-generate)
+
+
+-- Select an inner join on the artwork table using the artists' names 
+
+
+-- Run the previous query but eliminate duplicates 
+
+
+-- List all artwork titles and artists and add nationalities from artist table using a left inner join
+
+
+-- List all artwork titles and styles and add full_name and nationality from artist table using a right inner join
+
+
+-- Do an inner join on artwork using artists' names and group by artist with a total
+
+
+-- Use a subquery to look up artist id by last name and set all their artworks on_display to 1
+
