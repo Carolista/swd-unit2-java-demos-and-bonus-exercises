5d85f11b1676356a922db1a43d54b3c584e6fc90
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-23 12:29:57 -0400

    added starter code for SQL lesson 1 examples

---
+++ sql-queries/lesson-1-examples.sql
@@ -0,0 +1,67 @@
+/* 
+	Create table using SQL Workbench context menu 
+    instead of a script 
+*/
+
+# Add a record with all values
+
+
+-- Add a record with partial values
+
+
+-- Add 2 more records at the same time
+
+
+-- Add a new column, on_display, to the existing table
+
+
+-- Update the third record to indicate that it is on display
+
+
+-- Update the second record to assign the category and indicate that it is on display
+
+
+-- View all records in the table
+
+
+-- View only artwork currently on display
+
+
+-- Look up ID using name of artist
+
+
+-- Add a few more records
+
+
+-- View only records with ids greater than 5
+
+
+-- View titles, artist, and categories of all post-impressionist artwork
+
+
+-- Run a similar query as the last but only those NOT on display
+
+
+-- View titles and artist if artwork is by either da Vinci or Vermeer using OR
+
+
+-- Rewrite the previous query using IN
+
+
+-- View only records where the title begins with "The"
+
+
+-- View all records again, but order by style, ascending
+
+
+-- Delete "The Church at Auvers" record (use ID number)
+
+
+-- View all records, concatenating using an alias as follows: Title by Artist
+
+
+-- View the first three records
+
+
+-- View the next three records
+
