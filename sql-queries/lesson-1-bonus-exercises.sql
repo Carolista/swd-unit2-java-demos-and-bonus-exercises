dc20afa385a379a68f8ecf02c7d7041502340b01
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-02 17:55:04 -0400

    added lesson 1 bonus exercises for sql

---
+++ sql-queries/lesson-1-bonus-exercises.sql
@@ -0,0 +1,61 @@
+/*
+	SQL Lesson 1 Bonus Exercises
+
+	TODO: Create a new schema, sql_bonus_exercises
+    
+	TODO: Create a new table (use context menu), artist 
+		with the following columns:
+        - id (an integer that is a primary key, not null, unique)
+		- first_name (up to 30 chars)
+        - last_name (up to 30 chars)
+
+	Once schema and table exist, continue below
+*/
+
+-- TODO: Write and run a query to add the first record to the table: Leonardo da Vinci
+
+
+-- TODO: Write and run a query to add at least 9 more records to the table, for example: 
+--  Johannes Vermeer, Claude Monet, Gustav Klimt, Salvador Dalí, Vincent Van Gogh, etc.
+
+
+-- TODO: Write a query to return results for all records and run it
+
+
+-- TODO: Write and run a query to add a new column to the table: country (50 chars max)
+
+
+-- TODO: Write a query to change the country to the appropriate value for a single artist 
+--  using a conditional clause. Re-run the query with new values for each record 
+--  until they are all updated.
+
+
+-- TODO: Re-run the query you wrote above that shows results for all records
+
+-- TODO: Write and run a query that returns only the last name and country for all records
+
+
+-- TODO: Write and run a query that returns only records where the country is France 
+--  (or Italy or Netherlands or whatever works for your data)
+
+
+-- TODO: Write and run a query that returns only records where the first name begins with an L
+
+
+-- TODO: Write and run a query to view only the first 5 records
+
+
+-- TODO: Write and run a query to view only the next 5 records
+
+
+-- TODO: Write and run a query to delete one of the records
+
+
+-- TODO: Write and run a query to produce all records sorted by last name
+
+
+-- TODO: Write and run a query to produce all records sorted in reverse by country
+
+
+-- TODO: Write and run a query that provides a new temporary column called full_name 
+--  (instead of first and last names separately) along with country
