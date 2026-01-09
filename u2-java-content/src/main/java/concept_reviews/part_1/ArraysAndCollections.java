44ade772d52df5081733e1222963439c1ca132f5
joshliford <j_liford1@yahoo.com>
2026-01-08 21:47:29 -0600

    completed array and collections practice examples

--- u2-java-content/src/main/java/concept_reviews/part_1/ArraysAndCollections.java
+++ u2-java-content/src/main/java/concept_reviews/part_1/ArraysAndCollections.java
@@ -1,17 +1,29 @@
 package concept_reviews.part_1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArraysAndCollections {
+
+    public static void main(String[] args) {
+        printDesserts();
+        printSeasonsAndReasons();
+    }
 
     /* ARRAYS */
 
     public static void printDesserts() {
 
-        // TODO: Declare an empty array of four strings called favoriteDesserts.
+        // Declare an empty array of four strings called favoriteDesserts.
+        String[] favoriteDesserts = new String[4];
 
-        // TODO: Add your 4 favorite desserts one at a time to the array
+        // Add your 4 favorite desserts one at a time to the array
+        favoriteDesserts[0] = "Ice Cream";
+        favoriteDesserts[1] = "Sticky Toffee Pudding";
+        favoriteDesserts[2] = "Chocolate Chip Cookies";
+        favoriteDesserts[3] = "Apple Pie";
 
-
-        // TODO: Make use of a for-each loop to print something like this:
+        // Make use of a for-each loop to print something like this:
         /*
             My top 3 desserts:
               - dark chocolate covered pretzels
@@ -19,7 +31,10 @@ public class ArraysAndCollections {
               - chocolate cheesecake
               - apple pie
          */
-
+        System.out.println("My top 4 desserts:");
+        for (String dessert : favoriteDesserts) {
+            System.out.println("- " + dessert);
+        }
     }
 
 
@@ -27,13 +42,22 @@ public class ArraysAndCollections {
 
     public static void printSeasonsAndReasons() {
 
-        // TODO: Choose a collection type to store the four seasons of the year
-        //  along with your favorite thing about each season.
+        // Choose a collection type to store the four seasons of the year
+        // along with your favorite thing about each season.
+        HashMap<String, String> seasons = new HashMap<>();
+        seasons.put("Spring", "Nature awakens with blooming flowers and warming temperatures after winter's sleep.");
+        seasons.put("Summer", "Long, sunny days perfect for beach trips, barbecues, and outdoor adventures.");
+        seasons.put("Fall", "Crisp air and vibrant foliage create a cozy atmosphere as the year winds down.");
+        seasons.put("Winter", "Cold weather brings snowfall, holidays, and opportunities for indoor comfort and winter sports.");
 
+        // Print a series of sentences with the information on each season.
+        // Refactor your data if needed to fit the sentence format.
+        for (Map.Entry<String, String> season : seasons.entrySet()) {
+            String key = season.getKey();
+            String value = season.getValue();
 
-        // TODO: Print a series of sentences with the information on each season.
-        //  Refactor your data if needed to fit the sentence format.
+            System.out.println("In " + key + ": " + value);
 
+        }
     }
-
 }
--- u2-java-content/src/main/java/java_basics/ArrayExamples.java
+++ u2-java-content/src/main/java/java_basics/ArrayExamples.java
@@ -1,31 +1,45 @@
 package java_basics;
 
+import java.util.Arrays;
+
 public class ArrayExamples {
 
     public static void main(String[] args) {
 
         /* DECLARING AN EMPTY ARRAY */
 
-        // TODO: Declare an empty Array, hobbits, that can hold 8 elements of type String
+        // Declare an empty Array, hobbits, that can hold 8 elements of type String
+        String[] hobbits = new String[8];
 
-        // TODO: Add 4 Hobbits at different indices
+        // Add 4 Hobbits at different indices
+        hobbits[0] = "Bjorn";
+        hobbits[3] = "Gion";
+        hobbits[5] = "Balin";
+        hobbits[7] = "Smaug";
 
-        // TODO: Print an element at an index with an unassigned value
+        // Print an element at an index with an unassigned value
+        System.out.println(hobbits[1]); // output: null
 
-        // TODO: Print the array by name — what happens?
+        // Print the array by name — what happens?
+        System.out.println(hobbits); // output: memory address for hobbits ([Ljava.lang.String;@1d81eb93)
 
-        // TODO: Use ArrayExamples.toString() to print the array's contents
+        // Use ArrayExamples.toString() to print the array's contents
+        System.out.println(Arrays.toString(hobbits));
+        // output: [Bjorn, null, null, Gion, null, Balin, null, Smaug]
 
 
         /* INITIALIZING AN ARRAY WITH VALUES */
 
-        // TODO: Declare an array of integers, fellowship, representing the members of the Fellowship
+        // Declare an array of integers, fellowship, representing the members of the Fellowship
         // wizards, hobbits, elves, dwarves, men
+        int[] fellowship = {1, 4, 1, 1, 2};
 
-        // TODO: Print a sentence with the number of men
+        // Print a sentence with the number of men
+        System.out.println("Total number of men in the fellowship: " + fellowship[4]);
 
-        // TODO: Print the array elements using ArrayExamples.toString()
-
+        // Print the array elements using ArrayExamples.toString()
+        System.out.println(Arrays.toString(fellowship));
+        // output: [1, 4, 1, 1, 2]
     }
 
 }
