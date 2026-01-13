a85116919e79cab21eb43bebe631f733accda247
PuiSlate <pkaraket73@gmail.com>
2026-01-13 14:50:46 -0600

    practiced some Java exercises

--- u2-java-content/src/main/java/classes_and_objects/Book.java
+++ u2-java-content/src/main/java/classes_and_objects/Book.java
@@ -6,17 +6,65 @@ public class Book {
 
     // TODO: Declare private fields title, author, numPages, and available
     // available should be initialized to true
+    private String title;
+    private String author;
+    private int numPages;
+    private boolean available = true;
 
 
     // CONSTRUCTORS
 
     // TODO: Define a constructor that takes title, author, and numPages
+    public Book(String title, String author, int numPages) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+    }
 
-    // TODO: Define an alternate (overloaded) constructor that requires only title and author
 
 
     // GETTERS & SETTERS
 
     // TODO: Use the IntelliJ generate feature to create getters & setters
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void checkOut() {
+        this.setAvailable(false);
+        System.out.println(title + " has been checked out of the library");
+    }
+
+    public void checkIn() {
+        available = true;
+        System.out.println(title + " has been returned to the library");
+    }
 }
