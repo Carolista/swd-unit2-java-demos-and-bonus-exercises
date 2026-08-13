5ba1c1389d57ae042bd1f6a9a17a9b679f24d6dd
Carolista <cjones.renaissancewoman@gmail.com>
2026-08-13 13:44:43 -0400

    updated solution for part 3 for consistency with starter code

--- u2-java-content/src/main/java/concept_reviews/part_3/Main.java
+++ u2-java-content/src/main/java/concept_reviews/part_3/Main.java
@@ -3,11 +3,11 @@ package concept_reviews.part_3;
 import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         OrderService service = new OrderService();
 
         // Test 1: Happy Path
-        System.out.println("--- Test 1: Valid Execution ---");
+        System.out.println("--- Test 1: Valid Order ---");
         try {
             service.processOrder("ORD-1001", "order.json");
             System.out.println("[Main] Test 1 try - Order completed successfully.");
@@ -16,25 +16,26 @@ public class Main {
         }
 
         // Test 2: Unchecked Exception (InvalidOrderException)
-        System.out.println("\n--- Test 2: Handling Unchecked Exception ---");
+        System.out.println("\n--- Test 2: Unchecked Exception (Invalid Order ID) ---");
         try {
             service.processOrder("", "order.json");
         } catch (InvalidOrderException e) {
             System.out.println("[Main] Test 2 catch - Validation Error: " + e.getMessage());
-//        } catch (FileNotFoundException e) {
-//            // Required by compiler because processOrder declares 'throws FileNotFoundException'
-//            System.out.println("[Main] Test 2 catch - System Error: " + e.getMessage());
+        } catch (FileNotFoundException e) {
+            // Required by compiler because OrderRepository processOrder method declares
+            //  'throws FileNotFoundException' and OrderService class bubbles it up
+            System.out.println("[Main] Test 2 catch - System Error: " + e.getMessage());
         }
 
         // Test 3: Checked Exception Bubbling (FileNotFoundException) & Finally Block
-        System.out.println("\n--- Test 3: Bubbled Checked Exception & Finally ---");
+        System.out.println("\n--- Test 3: Checked Exception Bubbling (File Not Found) ---");
         try {
             // Exception originates in OrderRepository, bubbles through OrderService, caught here in Main
             service.processOrder("ORD-1002", "bad_file.txt");
         } catch (FileNotFoundException e) {
             System.out.println("[Main] Test 3 catch - System Error: " + e.getMessage());
         } finally {
-            System.out.println("[Main] Test 3 finally - Resource cleanup completed (always runs).");
+            System.out.println("[Main] Test 3 finally - Resource cleanup completed.");
         }
     }
 }
